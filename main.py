14ae1a13612e8461add6228297ed76e46005997b
Ong Jun Keat <ongjunkeat@gmail.com>
2018-01-19 18:05:58 +0800

    Now I know how to find mean of each row

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
+import csv
+import pandas as pd
+import math
 
-print("hello")
+data = pd.read_csv('price.csv')
+df = pd.DataFrame(data)
+# df.to_csv('new_price.csv')
+
+prices = []
+for index, row in  df.iterrows(): 
+    for i in range(6, len(row)):
+        if not math.isnan(row[i]):                    
+            prices.append(row[i])
+    print(sum(prices)/len(prices))
+    prices = []
+
+
+    
